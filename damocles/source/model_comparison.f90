dc6c436de85bb2177c5ec1113a6824a6b57f1cff
Antonia Bevan <antonia.bevan@gmail.com>
2018-02-07 11:35:46 +0000

    Amended chi2 error calculation & write out statements for future mcmc implementation

--- damocles/source/model_comparison.f90
+++ damocles/source/model_comparison.f90
@@ -24,7 +24,7 @@ contains
         !scale modelled fluxes and errors to data flux using observed line flux
         !tot_flux/(n_recorded_packets) = final average energy per packet.  Multiply by weight to get energy at end.
         profile_array_data_bins = line%tot_flux*total_weight_data_bins/n_recorded_packets
-        mc_error_data_bins = line%tot_flux*(square_weight_data_bins**0.5)/n_recorded_packets
+        mc_error_data_bins = line%tot_flux*(square_weight_data_bins**0.5)/(total_weight_data_bins*n_recorded_packets)
 
         !check whether velocity bins should be excluded from chi squared calculation (due to e.g. narrow line contamination)
         obs_data%exclude(:) = .false.
